32bb5dda38e02ce375a0ede6172ec75a05bd5737
David Byrne <byrne_david@outlook.com>
2019-12-12 17:32:53 +0000

    fixed sorting by year/upvotes

--- src/App.js
+++ src/App.js
@@ -9,31 +9,16 @@ import Form from "./components/newsForm"
 
 export default class App extends Component {
 
-  state = {posts: [{}]};
+  state = {posts: [{}], sort:"votes" };
 
   componentDidMount() {
       api.getAll().then(resp => {
           this.setState({
-              posts: resp.posts
+              posts: resp.posts, sort: "votes"
           });
       }).catch(console.error);
   };
 
-  /*
-  constructor(props){
-    super(props)
-    this.state = {
-      sort: "votes"
-    }
-  }
- */
-
- /*
-  incrementUpvote = id => {
-    api.upvote(id);
-    this.setState({});
-  };
-  */
 
  incrementUpvote = (id) => {
   api.upvote(id).then(resp=> {
@@ -46,26 +31,11 @@ export default class App extends Component {
   addNewsItem = (title, link) => {
     api.add(title,link)
     .then(resp => {
-                  const newPost = {"id":resp.id,"title":title,"link":link,"upvotes":0, "comments":[]};
+                  const newPost = {"id":resp.id,"title":title,"link":link,"upvotes":0,"comments":[]};
                   this.setState({posts: this.state.posts.concat([newPost])});
     })
   };
 
-  /*
-  addNewsItem = (title, year, author, link) => {
-    api.add(title, year, author, link);
-    this.setState({});
-  };
- */
-
- /*
-  removeNewsItem = id => {
-    api.removePost(id);
-    this.setState({});
-  };
-*/
-
-/*
   sort = () => {
     if(this.state.sort === "votes")      {
       this.setState({sort:"year"});
@@ -73,14 +43,24 @@ export default class App extends Component {
       this.setState({sort:"votes"})
     }
   };
-  */
+  
 
 
  render() {
-  const posts = _.sortBy(this.state.posts, post =>
-      post.upvotes);
+  let posts;
+  if(this.state.sort === "votes"){
+   posts = _.sortBy(this.state.posts, post => post.upvotes);
+  }else{
+    posts = _.sortBy(this.state.posts, post => post.year);
+  }
+
   return (
       <div className="container">
+        <div className="colo-md-8 offset-2">
+          <p></p>
+        <button className="btn btn-dark" onClick={this.sort}>Sort year/upvotes</button>
+        <p></p>
+        </div>
           <div className="row">
               <div className="col-md-9 col-md-offset-1">
                   <NewsList posts={posts} 
@@ -96,36 +76,4 @@ export default class App extends Component {
   );
 }
 
-
-/*
-  render() {
-    let posts;
-    if(this.state.sort === "votes"){
-     posts = _.sortBy(api.getAll(), post => -post.upvotes);
-    }else{
-      posts = _.sortBy(api.getAll(), post => -post.year);
-    }
-
-    return (
-      <div className="container-fluid" class="backClr">
-        <div className="row">
-          <div className="col-md-8 offset-2 ">
-          <p></p>
-          <button className="btn btn-dark buttnWidth"><Link to={"/addBook"}>Add Book</Link></button>
-          </div>
-        </div>
-        <div className="colo-md-8 offset-2">
-          <p></p>
-        <button className="btn btn-dark" onClick={this.sort}>Sort year/upvotes</button>
-        <p></p>
-        </div>
-        <div>
-          <div className="col-md-8 offset-2">
-            <NewsList posts={posts} upvoteHandler={this.incrementUpvote} removeHandler={this.removeNewsItem} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-  */
 }
